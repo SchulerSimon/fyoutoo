7ade9b08e82c3c3325a401c017ec32b8d98d3f48
Simon Schuler <simon.schuler@vivai.de>
2023-10-19 08:40:26 +0200

    move gui into main() method

--- youfucktubegui.py
+++ youfucktubegui.py
@@ -1,60 +1,64 @@
 import tkinter as tk
 import src.youfucktube as youfucktube
 
-app = tk.Tk()
-app.title("YouFuckTubeGUI")
-app.geometry("400x150")
-app.configure(bg="#252526")
 
-normal_font = ("Helvetica", 10)
-bold_font = ("Helvetica", 12, "bold")
+def main():
+    app = tk.Tk()
+    app.title("YouFuckTubeGUI")
+    app.geometry("400x150")
+    app.configure(bg="#252526")
 
-spacer_label = tk.Label(app, text="", bg="#252526").pack()
+    normal_font = ("Helvetica", 10)
+    bold_font = ("Helvetica", 12, "bold")
 
-input_label = tk.Label(
-    app, text="Youtube URL:", font=bold_font, fg="white", bg="#252526"
-)
-input_label.pack()
+    spacer_label = tk.Label(app, text="", bg="#252526").pack()
 
-url = tk.Entry(app, width=50, font=normal_font)
-url.pack()
+    input_label = tk.Label(
+        app, text="Youtube URL:", font=bold_font, fg="white", bg="#252526"
+    )
+    input_label.pack()
 
-err_msg = tk.StringVar()
+    url = tk.Entry(app, width=50, font=normal_font)
+    url.pack()
 
+    err_msg = tk.StringVar()
 
-def button_press():
-    video_url: str = url.get()
+    spacer_label2 = tk.Label(app, text="", bg="#252526").pack()
 
-    err_msg.set("")
+    def button_press():
+        video_url: str = url.get()
 
-    try:
-        video_id: str = youfucktube.extract_video_id(video_url)
-    except ValueError:
-        err_msg.set("Please input a valid youtube URL.")
+        err_msg.set("")
+
+        try:
+            video_id: str = youfucktube.extract_video_id(video_url)
+        except ValueError:
+            err_msg.set("Please input a valid youtube URL.")
+            return
+
+        link = youfucktube.create_link(video_id)
+        youfucktube.open_browser(link)
+
         return
 
-    link = youfucktube.create_link(video_id)
-    youfucktube.open_browser(link)
+    submit_button = tk.Button(
+        app,
+        text="Submit",
+        command=button_press,
+        font=bold_font,
+        bg="#007acc",
+        fg="white",
+        borderwidth=1,
+    )
+    submit_button.pack()
 
-    return
+    warning_label = tk.Label(
+        app, textvariable=err_msg, fg="#dadf49", bg="#252526", font=normal_font
+    )
+    warning_label.pack()
+
+    app.mainloop()
 
 
-spacer_label2 = tk.Label(app, text="", bg="#252526").pack()
-
-submit_button = tk.Button(
-    app,
-    text="Submit",
-    command=button_press,
-    font=bold_font,
-    bg="#007acc",
-    fg="white",
-    borderwidth=1,
-)
-submit_button.pack()
-
-warning_label = tk.Label(
-    app, textvariable=err_msg, fg="#dadf49", bg="#252526", font=normal_font
-)
-warning_label.pack()
-
-app.mainloop()
+if __name__ == "__main__":
+    main()
