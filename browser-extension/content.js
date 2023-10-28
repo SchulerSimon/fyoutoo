ef8068f05e3600e96d35814f4817ce53e0c93028
Maxime Veit <p@mveit.de>
2023-10-29 00:06:35 +0200

    Bugfix (reload hotfix not required anymore)

--- browser-extension/content.js
+++ browser-extension/content.js
@@ -1,25 +1,19 @@
 (function() {
-	var v = new URLSearchParams(window.location.search).get('v');
-	var pathName = window.location.pathname;
-	var i = setInterval(function() {
-		// Only if ready
-		if (document.readyState !== 'complete') {
+	let v = undefined;
+	setInterval(function() {
+
+		// Only if ready, movie_player div loaded and when watching a video
+		if (document.readyState !== 'complete' || ! document.getElementById("movie_player") || window.location.pathname != "/watch") {
 			return;
 		}
-		// Only interested if we watch a video or come from a video
-		if (window.location.pathname != "/watch" && pathName != "/watch") {
-			return;
-		}
-		// Reload page if video changes (hotfix, can't stop playing video)
-		if (v != new URLSearchParams(window.location.search).get('v')) {
-			location.reload();
-		}
-		// Wait until movie player is there
-		if (!document.getElementById("movie_player")) {
-			return;
-		}
-		// Check if player is still there
-		if (!document.getElementById("youfucktube-player")) {
+
+		// Stop all videos on page
+                Array.from(document.getElementsByTagName("video")).forEach(e => e.pause);
+
+		// Place player (or update if video has changed)
+                let v2 = new URLSearchParams(window.location.search).get('v');
+		if (!document.getElementById("youfucktube-player") || v != v2) {
+                        v = v2;
                         let iframe = document.createElement('iframe');
                         iframe.id = "youfucktube-player";
                         iframe.style = "border-left: 2px solid darkred; width: 100%; height: 100%;";
@@ -29,7 +23,8 @@
                         iframe.allowfullscreen = "1";
                         iframe.src = "https://www.youtube.com/embed/" + v;
 			document.getElementById("movie_player").innerHTML = iframe.outerHTML;
+                        console.log("youfucktube-player placed");
 		}
 
-	}, 1000);
+	}, 500);
 })()
